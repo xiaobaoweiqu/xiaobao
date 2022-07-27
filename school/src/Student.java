e0750b6b8e569d984279963ce1224f5740d99bea
jianisme159357 <jiantianyang13@gmail.com>
2022-07-27 10:09:46 +0800

    这是小包

---
+++ school/src/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    //做广播体操
+    //上课
+    //阅读
+    //跑步
+    //f(x) =ax+b
+    public void  gotoExercise(){
+        System.out.println("我们在认真的做广播体操");
+
+    }
+
+    public void  gotoClass() {
+        System.out.println("我们在认真的上课");
+    }
+
+    public void gotoRead(){
+        System.out.println("我们在认真的阅读");
+
+    }
+    public void gotoRun(){
+        System.out.println("我们在认真的跑步");
+    }
+}
